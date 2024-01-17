a1fcf61d28d50f90cdcba84c3be55f133a9ed244
Sachin Kumar Dhuriya <110597225+dhuriyaSachin2000@users.noreply.github.com>
2024-01-17 21:55:22 +0530

    Reverse The Array

---
+++ reverseTheArray.cpp
@@ -0,0 +1,11 @@
+void reverseArray(vector<int> &arr , int m)
+    // Write your code here   
+    {
+        int s=m+1, e=arr.size()-1;
+        while(s<=e){
+            swap(arr[s],arr[e]);
+            s++;
+            e--;
+        }
+    }
+        
