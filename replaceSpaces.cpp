3535d4ea577c5ca126fe13d76f6a9c733a55b788
Sachin Kumar Dhuriya <110597225+dhuriyaSachin2000@users.noreply.github.com>
2024-01-30 17:27:30 +0530

    Replace spaces in a sentence

---
+++ replaceSpaces.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+string replaceSpaces(string &str){
+	// Write your code here.
+	string temp = "";
+	int i=0;
+	for(i=0; i<str.length(); i++){
+		if(str[i] == ' '){
+			temp.push_back('@');
+			temp.push_back('4');
+			temp.push_back('0');
+		}
+		else
+		{
+			temp.push_back(str[i]);
+		}
+	}
+	return temp;
+}
